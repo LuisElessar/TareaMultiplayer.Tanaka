b0f61a4a10d881bfe3c3b4cc365af952336a9425
LuisElessar <SteveLuis4555@gmail.com>
2024-12-05 08:02:06 -0500

    Enemigos reciben y hacen daño

--- TareaMultijugadorTanaka/Assets/Scripts/Enemy.cs
+++ TareaMultijugadorTanaka/Assets/Scripts/Enemy.cs
@@ -7,18 +7,23 @@ public class Enemy : MonoBehaviourPun
 {
     public int maxHealth = 100;
     private int currentHealth;
-    private Transform player; 
 
-    public float speed = 10f; 
+    private Transform player;
+
+    public float speed = 10f;
+    public float damage = 10f;
+    public float attackRate = 1f;
+    public float nextAttack = 0f;
+    public float attackRange = 1.5f;
 
     private void Start()
     {
         currentHealth = maxHealth;
 
-       
+
         if (PhotonNetwork.IsConnected && PhotonNetwork.LocalPlayer != null)
         {
-            player = Player.LocalInstance.transform; 
+            player = Player.LocalInstance.transform;
         }
     }
 
@@ -26,9 +31,38 @@ public class Enemy : MonoBehaviourPun
     {
         if (player != null)
         {
-            
-            Vector3 direction = (player.position - transform.position).normalized;
-            transform.position += direction * speed * Time.deltaTime;
+
+            MovetoPlayer();
+            TryAttackPlayer();
+        }
+    }
+
+    private void MovetoPlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void TryAttackPlayer()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer <= attackRange && Time.time >= nextAttack)
+        {
+            nextAttack = Time.time + attackRate; 
+            photonView.RPC("AttackPlayer", RpcTarget.All, damage);
+        }
+    }
+
+    [PunRPC] 
+    private void AttackPlayer(float damage)
+    {
+        if (player != null)
+        {
+            Player playerScript = player.GetComponent<Player>();
+            if (playerScript != null)
+            {
+                playerScript.TakeDamage((int)damage);
+            }
         }
     }
 
